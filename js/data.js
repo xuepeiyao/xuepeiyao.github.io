8d247ea87364c8b38c9177c7a58a396e834b71f5
LeonardLT <leonardlt@outlook.com>
2018-05-30 06:34:02 +0800

    add index photo

---
+++ js/data.js
@@ -0,0 +1,108 @@
+var indexImgs = [
+    [
+        {
+            imgUrl: './images/communication/one/index.png',
+            imgTitle: '环境导视系统',
+            linkUrl: 'detail.html'
+        },
+        {
+            imgUrl: './images/communication/two/index.png',
+            imgTitle: '捷业独角兽',
+            linkUrl: 'detail.html'
+
+        },
+        {
+            imgUrl: './images/communication/three/index.png',
+            imgTitle: '空指针乐队',
+            linkUrl: 'detail.html'
+
+        },
+        {
+            imgUrl: './images/communication/four/index.png',
+            imgTitle: '像素小人',
+            linkUrl: 'detail.html'
+
+        },
+        {
+            imgUrl: './images/communication/five/index.png',
+            imgTitle: '壹庐茶社',
+            linkUrl: 'detail.html'
+
+        },
+        {
+            imgUrl: './images/communication/six/index.png',
+            imgTitle: 'TED X',
+            linkUrl: 'detail.html',
+
+        },
+        {
+            imgUrl: './images/communication/seven/index.png',
+            imgTitle: '宏欣教育',
+            linkUrl: 'detail.html'
+        }
+    ],
+    [
+        {
+            imgUrl: './images/ui-design/one/index.png',
+            imgTitle: '华阳景区',
+            linkUrl: 'detail.html'
+        },
+        {
+            imgUrl: './images/ui-design/two/index.png',
+            imgTitle: '西安音乐厅文艺账单',
+            linkUrl: 'detail.html'
+        },
+        {
+            imgUrl: './images/ui-design/three/index.png',
+            imgTitle: '长安嘿帮',
+            linkUrl: 'detail.html'
+        },
+        {
+            imgUrl: './images/ui-design/four/index.png',
+            imgTitle: 'D.E.A.L',
+            linkUrl: 'detail.html'
+        },
+
+        {
+            imgUrl: './images/ui-design/five/index.png',
+            imgTitle: '飞跃大西安',
+            linkUrl: 'detail.html'
+        }
+
+    ],
+    [
+        {
+            imgUrl: './images/web/one/index.png',
+            imgTitle: '博达官网',
+            linkUrl: 'detail.html'
+        },
+        {
+            imgUrl: './images/web/two/index.png',
+            imgTitle: '天津大学教师个人主页',
+            linkUrl: 'detail.html'
+        },
+        {
+            imgUrl: './images/web/three/index.png',
+            imgTitle: '西安音乐厅乐团',
+            linkUrl: 'detail.html'
+        }
+    ],
+    [
+        {
+            imgUrl: './images/poster-design/one/index.png',
+            imgTitle: '城墙海报',
+            linkUrl: 'detail.html'
+        },
+        {
+            imgUrl: './images/poster-design/two/index.png',
+            imgTitle: '海报',
+            linkUrl: 'detail.html'
+        },
+        {
+            imgUrl: './images/poster-design/three/index.png',
+            imgTitle: '我是赫拉',
+            linkUrl: 'detail.html'
+        }
+    ]
+
+]
