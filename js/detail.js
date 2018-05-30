b51118e4beae48bf061ba6d0df5af9c57223c0e3
LeonardLT <leonardlt@outlook.com>
2018-05-31 07:17:34 +0800

    fixed index css

---
+++ js/detail.js
@@ -0,0 +1,10 @@
+var ddetailData = [
+    {
+        title: "TEDx XI’AN",
+        description: "TEDxXi’an是一个非营利的公益性组织，自己有幸成为其中的一名志愿者，负责大会的整体的视觉海报及延伸品，视觉组的小伙伴都很有才，各尽其能，在这个大家庭里，没有甲方的干预，可以大胆的解放天性，大开脑洞。" +
+        "TEDxXi'an is a nonprofit public service organization, have the opportunity to become a volunteer yourself which, overall visual poster and extension products, a small group of partners in charge of the General Assembly of the visual is very talented, do their best, in this family, no party intervention can boldly liberate nature, wide open brain hole.",
+        imgs: [
+            ""
+        ]
+    }
+]
