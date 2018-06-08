5821607b4d0abce82f6b6f69bdbc4d40362f9995
ltjn <364125168@qq.com>
2018-06-08 14:35:02 +0800

    change index img order

--- js/data.js
+++ js/data.js
@@ -1,164 +1,178 @@
+
 var indexImgs = [
-    [
         {
+        	genre:'posterDesign',
+            imgUrl: './images/poster-design/one/index.png',
+            imgTitle: '西安城墙海报',
+            linkUrl: 'detail.html?id=15'
+        },
+        {
+        	genre:'web',
+            imgUrl: './images/web/three/index.png',
+            imgTitle: '西安音乐厅乐团',
+            linkUrl: 'detail.html?id=14'
+        },
+        {
+        	genre:'uiDesign',
+            imgUrl: './images/ui-design/two/index.png',
+            imgTitle: '西安音乐厅文艺账单',
+            linkUrl: 'detail.html?id=8'
+        },
+        {
+        	genre:'communication',
+            imgUrl: './images/communication/six/index.png',
+            imgTitle: 'TED X',
+            linkUrl: 'detail.html?id=5'
+        },
+        {
+        	genre:'uiDesign',
+            imgUrl: './images/ui-design/one/index.png',
+            imgTitle: '华阳景区',
+            linkUrl: 'detail.html?id=7'
+        },
+        {
+        	genre:'communication',
             imgUrl: './images/communication/one/index.png',
             imgTitle: '环境导视系统',
             linkUrl: 'detail.html?id=0'
         },
         {
+        	genre:'communication',
+            imgUrl: './images/communication/seven/index.png',
+            imgTitle: '宏欣教育',
+            linkUrl: 'detail.html?id=6'
+        },
+        {
+        	genre:'uiDesign',
+            imgUrl: './images/ui-design/five/index.png',
+            imgTitle: '飞跃大西安',
+            linkUrl: 'detail.html?id=11'
+        },
+        {
+        	genre:'web',
+            imgUrl: './images/web/one/index.png',
+            imgTitle: '博达官网',
+            linkUrl: 'detail.html?id=12'
+        },
+        {
+        	genre:'web',
+            imgUrl: './images/web/two/index.png',
+            imgTitle: '天津大学教师个人主页',
+            linkUrl: 'detail.html?id=13'
+        },
+        {
+        	genre:'communication',
             imgUrl: './images/communication/two/index.png',
             imgTitle: '捷业独角兽',
             linkUrl: 'detail.html?id=1'
 
         },
         {
+        	genre:'communication',
             imgUrl: './images/communication/three/index.png',
             imgTitle: '空指针乐队',
             linkUrl: 'detail.html?id=2'
-
         },
         {
-            imgUrl: './images/communication/four/index.png',
-            imgTitle: '像素小人',
-            linkUrl: 'detail.html?id=3'
-
-        },
-        {
-            imgUrl: './images/communication/five/index.png',
-            imgTitle: '壹庐茶社',
-            linkUrl: 'detail.html?id=4'
-
-        },
-        {
-            imgUrl: './images/communication/six/index.png',
-            imgTitle: 'TED X',
-            linkUrl: 'detail.html?id=5'
-
-        },
-        {
-            imgUrl: './images/communication/seven/index.png',
-            imgTitle: '宏欣教育',
-            linkUrl: 'detail.html?id=6'
-        }
-    ],
-    [
-        {
-            imgUrl: './images/ui-design/one/index.png',
-            imgTitle: '华阳景区',
-            linkUrl: 'detail.html?id=7'
-        },
-        {
-            imgUrl: './images/ui-design/two/index.png',
-            imgTitle: '西安音乐厅文艺账单',
-            linkUrl: 'detail.html?id=8'
-        },
-        {
+        	genre:'uiDesign',
             imgUrl: './images/ui-design/three/index.png',
             imgTitle: '长安嘿帮',
             linkUrl: 'detail.html?id=9'
         },
         {
+        	genre:'communication',
+            imgUrl: './images/communication/four/index.png',
+            imgTitle: '像素小人',
+            linkUrl: 'detail.html?id=3'
+        },
+        {
+        	genre:'posterDesign',
+            imgUrl: './images/poster-design/three/index.png',
+            imgTitle: '我是赫拉话剧海报',
+            linkUrl: 'detail.html?id=17'
+        },
+        {
+        	genre:'communication',
+            imgUrl: './images/communication/five/index.png',
+            imgTitle: '壹庐茶社',
+            linkUrl: 'detail.html?id=4'
+        },
+        {
+        	genre:'uiDesign',
             imgUrl: './images/ui-design/four/index.png',
             imgTitle: 'D.E.A.L',
             linkUrl: 'detail.html?id=10'
         },
-
         {
-            imgUrl: './images/ui-design/five/index.png',
-            imgTitle: '飞跃大西安',
-            linkUrl: 'detail.html?id=11'
-        }
-    ],
-    [
-        {
-            imgUrl: './images/web/one/index.png',
-            imgTitle: '博达官网',
-            linkUrl: 'detail.html?id=12'
-        },
-        {
-            imgUrl: './images/web/two/index.png',
-            imgTitle: '天津大学教师个人主页',
-            linkUrl: 'detail.html?id=13'
-        },
-        {
-            imgUrl: './images/web/three/index.png',
-            imgTitle: '西安音乐厅乐团',
-            linkUrl: 'detail.html?id=14'
-        }
-    ],
-    [
-        {
-            imgUrl: './images/poster-design/one/index.png',
-            imgTitle: '西安城墙海报',
-            linkUrl: 'detail.html?id=15'
-        },
-        {
+        	genre:'posterDesign',
             imgUrl: './images/poster-design/two/index.png',
             imgTitle: '海报',
             linkUrl: 'detail.html?id=16'
         },
         {
-            imgUrl: './images/poster-design/three/index.png',
-            imgTitle: '我是赫拉话剧海报',
-            linkUrl: 'detail.html?id=17'
-        }
-    ],
-    [
-        {
+        	genre:'illustion',
             imgUrl: './images/illustion/1_index.png',
-            imgTitle: '插画',
+            imgTitle: '街角 （上）',
             linkUrl: 'detail.html?id=18'
         },
         {
+        	genre:'illustion',
+            imgUrl: './images/illustion/9_index.png',
+            imgTitle: '街角 （下）',
+            linkUrl: 'detail.html?id=26'
+        },
+        {
+        	genre:'illustion',
             imgUrl: './images/illustion/2_index.png',
-            imgTitle: '插画',
+            imgTitle: '下午茶',
             linkUrl: 'detail.html?id=19'
         },
         {
+        	genre:'illustion',
             imgUrl: './images/illustion/3_index.png',
-            imgTitle: '插画',
+            imgTitle: '哦 噗嗤',
             linkUrl: 'detail.html?id=20'
         },
         {
+        	genre:'illustion',
             imgUrl: './images/illustion/4_index.png',
             imgTitle: '插画',
             linkUrl: 'detail.html?id=21'
         },
         {
+        	genre:'illustion',
             imgUrl: './images/illustion/5_index.png',
-            imgTitle: '插画',
+            imgTitle: '蘑菇人',
             linkUrl: 'detail.html?id=22'
         },
         {
+        	genre:'illustion',
             imgUrl: './images/illustion/6_index.png',
-            imgTitle: '插画',
+            imgTitle: '啊',
             linkUrl: 'detail.html?id=23'
         },
         {
+        	genre:'illustion',
             imgUrl: './images/illustion/7_index.png',
-            imgTitle: '插画',
+            imgTitle: '随笔（上）',
             linkUrl: 'detail.html?id=24'
         },
         {
+        	genre:'illustion',
             imgUrl: './images/illustion/8_index.png',
-            imgTitle: '插画',
+            imgTitle: '随笔（下）',
             linkUrl: 'detail.html?id=25'
         },
         {
-            imgUrl: './images/illustion/9_index.png',
-            imgTitle: '插画',
-            linkUrl: 'detail.html?id=26'
-        },
-        {
+        	genre:'illustion',
             imgUrl: './images/illustion/10_index.png',
-            imgTitle: '插画',
+            imgTitle: '井上雄彦',
             linkUrl: 'detail.html?id=27'
         },
         {
+        	genre:'illustion',
             imgUrl: './images/illustion/11_index.png',
-            imgTitle: '插画',
+            imgTitle: '自画像',
             linkUrl: 'detail.html?id=28'
-        },
-    ]
-
-]
+        }
+];
