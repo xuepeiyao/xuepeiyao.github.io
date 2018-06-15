349def08a3e2d827b7af889eb9f3bb76f3aafb9c
ltjn <364125168@qq.com>
2018-06-15 18:38:06 +0800

    index bg

--- js/data.js
+++ js/data.js
@@ -5,213 +5,398 @@ var indexImgs = [
             imgUrl: './images/poster-design/one/index.png',
             imgTitle: '西安城墙海报',
             linkUrl: 'detail.html?id=15',
-            bgColor3:'rgb(210, 228, 245)',
+            bg3_color:'rgb(210, 228, 245)',
             bg1_color:'rgb(35, 78, 158)',
-            bg1_height:'50%',
-            bg1_width:'100%',
             bg2_color:'rgb(119, 167, 217)',
+            bg1_height:'50%',//版式3
+            bg1_width:'100%',
             bg2_height:'50%',
+            bg2_width:'100%',
+            bg2_left:'0%',
+            bg2_top:'50%',
         },
         {
             genre:'web',
             imgUrl: './images/web/three/index.png',
             imgTitle: '西安音乐厅乐团',
             linkUrl: 'detail.html?id=14',
-            bgColor3:'rgb(222, 226, 230)',
-            bg1_color:'rgb(62, 60, 61)',
+            bg3_color:'rgb(242,242,242)',
+            bg1_color:'rgb(182,39,45)',
             bg1_height:'50%',
-            bg2_color:'rgb(180, 39, 46)',
-            bg2_height:'100%',
+            bg1_width:'100%',
+            bg2_color:'rgb(36, 28, 21)',//版式1
+            bg2_height:'50%',
+            bg2_top:'50%',
         },
         {
             genre:'uiDesign',
             imgUrl: './images/ui-design/two/index.png',
             imgTitle: '西安音乐厅文艺账单',
             linkUrl: 'detail.html?id=8',
-            bgColor3:'rgb(222, 226, 230)',
-            bg1_color:'rgb(35, 172, 59)',
-            bg1_height:'100%',
-            bg2_color:'rgb(36, 28, 21)',
+            bg3_color:'rgb(202,179,139)',
+            bg1_color:'rgb(40,82,77)',
+            bg1_height:'50%',
+            bg1_width:'100%',
+            bg2_color:'rgb(36, 28, 21)',//版式1
             bg2_height:'50%',
+            bg2_top:'50%',
         },
         {
         	genre:'communication',
             imgUrl: './images/communication/six/index.png',
             imgTitle: 'TED X',
             linkUrl: 'detail.html?id=5',
-            bgColor3:'rgb(41, 32, 27)',
-            bg1_color:'rgb(228, 231, 237)',
-            bg1_height:'100%',
-            bg2_color:'rgb(148, 144, 146)',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(230,33,17)',
+            bg1_height:'100%',//版式2
+            bg2_color:'rgb(0,160,233)',
             bg2_height:'100%',
+            bg2_top:'0%',
         },
         {
         	genre:'uiDesign',
             imgUrl: './images/ui-design/one/index.png',
             imgTitle: '华阳景区',
-            linkUrl: 'detail.html?id=7'
+            linkUrl: 'detail.html?id=7',
+            bg3_color:'rgb(243,237,229)',
+            bg1_color:'rgb(229,197,151)',
+            bg1_height:'50%',
+            bg1_width:'100%',
+            bg2_color:'rgb(106,73,72)',//版式1
+            bg2_height:'50%',
+            bg2_top:'50%',
         },
         {
         	genre:'communication',
             imgUrl: './images/communication/one/index.png',
             imgTitle: '环境导视系统',
-            linkUrl: 'detail.html?id=0'
+            linkUrl: 'detail.html?id=0',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(83,131,192)',
+            bg2_color:'rgb(232,233,234)',
+            bg1_height:'50%',//版式3
+            bg1_width:'100%',
+            bg2_height:'50%',
+            bg2_width:'100%',
+            bg2_left:'0%',
+            bg2_top:'50%',
         },
         {
         	genre:'communication',
             imgUrl: './images/communication/seven/index.png',
             imgTitle: '宏欣教育',
-            linkUrl: 'detail.html?id=6'
+            linkUrl: 'detail.html?id=6',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(232,233,234)',
+            bg1_height:'100%',//版式2
+            bg2_color:'rgb(31,161,232)',
+            bg2_height:'100%',
+            bg2_top:'0%',
         },
         {
         	genre:'uiDesign',
             imgUrl: './images/ui-design/five/index.png',
             imgTitle: '飞跃大西安',
-            linkUrl: 'detail.html?id=11'
+            linkUrl: 'detail.html?id=11',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(49,76,153)',
+            bg1_height:'100%',//版式2
+            bg2_color:'rgb(119,164,213)',
+            bg2_height:'100%',
+            bg2_top:'0%',
         },
         {
         	genre:'web',
             imgUrl: './images/web/one/index.png',
             imgTitle: '博达官网',
-            linkUrl: 'detail.html?id=12'
+            linkUrl: 'detail.html?id=12',
+            bg3_color:'rgb(227,231,236)',
+            bg1_color:'rgb(196,197,201)',
+            bg1_height:'50%',
+            bg1_width:'100%',
+            bg2_color:'rgb(0,68,155)',//版式1
+            bg2_height:'50%',
+            bg2_top:'50%',
         },
         {
         	genre:'web',
             imgUrl: './images/web/two/index.png',
             imgTitle: '天津大学教师个人主页',
-            linkUrl: 'detail.html?id=13'
+            linkUrl: 'detail.html?id=13',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(212,212,219)',
+            bg1_height:'100%',//版式2
+            bg2_color:'rgb(114,83,73)',
+            bg2_height:'100%',
+            bg2_top:'0%',
         },
         {
         	genre:'communication',
             imgUrl: './images/communication/two/index.png',
             imgTitle: '捷业独角兽',
-            linkUrl: 'detail.html?id=1'
-
+            linkUrl: 'detail.html?id=1',
+            bg3_color:'rgb(237,98,121)',//版式4
         },
         {
         	genre:'communication',
             imgUrl: './images/communication/three/index.png',
             imgTitle: '空指针乐队',
-            linkUrl: 'detail.html?id=2'
+            linkUrl: 'detail.html?id=2',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(227,179,50)',
+            bg1_height:'100%',//版式2
+            bg2_color:'rgb(239,231,230)',
+            bg2_height:'100%',
+            bg2_top:'0%',
         },
         {
         	genre:'uiDesign',
             imgUrl: './images/ui-design/three/index.png',
             imgTitle: '长安嘿帮',
-            linkUrl: 'detail.html?id=9'
+            linkUrl: 'detail.html?id=9',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(36,39,54)',
+            bg2_color:'rgb(217,218,222)',
+            bg1_height:'50%',//版式3
+            bg1_width:'100%',
+            bg2_height:'50%',
+            bg2_width:'100%',
+            bg2_left:'0%',
+            bg2_top:'50%',
         },
         {
         	genre:'communication',
             imgUrl: './images/communication/four/index.png',
             imgTitle: '像素小人',
-            linkUrl: 'detail.html?id=3'
+            linkUrl: 'detail.html?id=3',
+            bg3_color:'rgb(244,234,232)',
+            bg1_color:'rgb(2,253,255)',
+            bg1_height:'50%',
+            bg1_width:'100%',
+            bg2_color:'rgb(251,176,59)',//版式1
+            bg2_height:'50%',
+            bg2_top:'50%',
         },
         {
         	genre:'posterDesign',
             imgUrl: './images/poster-design/three/index.png',
             imgTitle: '我是赫拉话剧海报',
-            linkUrl: 'detail.html?id=17'
+            linkUrl: 'detail.html?id=17',
+            bg3_color:'rgb(191,164,213)',//版式4
         },
         {
         	genre:'communication',
             imgUrl: './images/communication/five/index.png',
             imgTitle: '壹庐茶社',
-            linkUrl: 'detail.html?id=4'
+            linkUrl: 'detail.html?id=4',
+            bg3_color:'rgb(243,237,229)',
+            bg1_color:'rgb(229,197,151)',
+            bg1_height:'50%',
+            bg1_width:'100%',
+            bg2_color:'rgb(106,73,72)',//版式1
+            bg2_height:'50%',
+            bg2_top:'50%',
         },
         {
         	genre:'uiDesign',
             imgUrl: './images/ui-design/four/index.png',
             imgTitle: 'D.E.A.L',
-            linkUrl: 'detail.html?id=10'
+            linkUrl: 'detail.html?id=10',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(36,39,54)',
+            bg2_color:'rgb(217,218,222)',
+            bg1_height:'50%',//版式3
+            bg1_width:'100%',
+            bg2_height:'50%',
+            bg2_width:'100%',
+            bg2_left:'0%',
+            bg2_top:'50%',
         },
         {
         	genre:'posterDesign',
             imgUrl: './images/poster-design/two/index.png',
             imgTitle: '海报',
-            linkUrl: 'detail.html?id=16'
+            linkUrl: 'detail.html?id=16',
+            bg3_color:'rgb(243,237,229)',
+            bg1_color:'rgb(229,197,151)',
+            bg1_height:'50%',
+            bg1_width:'100%',
+            bg2_color:'rgb(106,73,72)',//版式1
+            bg2_height:'50%',
+            bg2_top:'50%',
         },
         {
             genre:'communication',
             imgUrl: './images/communication/eight/index.png',
             imgTitle: 'send a letter',
-            linkUrl: 'detail.html?id=29'
+            linkUrl: 'detail.html?id=29',
+            bg3_color:'rgb(212,212,219)',//版式4
         },
         {
             genre:'communication',
             imgUrl: './images/communication/nine/index.png',
             imgTitle: '梦长安',
-            linkUrl: 'detail.html?id=30'
+            linkUrl: 'detail.html?id=30',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(36,39,54)',
+            bg2_color:'rgb(217,218,222)',
+            bg1_height:'50%',//版式3
+            bg1_width:'100%',
+            bg2_height:'50%',
+            bg2_width:'100%',
+            bg2_left:'0%',
+            bg2_top:'50%',
         },
         {
             genre:'communication',
             imgUrl: './images/communication/ten/index.png',
             imgTitle: '字体实验',
-            linkUrl: 'detail.html?id=31'
+            linkUrl: 'detail.html?id=31',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(36,39,54)',
+            bg1_height:'100%',//版式2
+            bg2_color:'rgb(217,218,222)',
+            bg2_height:'100%',
+            bg2_top:'0%',
         },
         {
         	genre:'illustion',
             imgUrl: './images/illustion/1_index.png',
             imgTitle: '街角 （上）',
-            linkUrl: 'detail.html?id=18'
+            linkUrl: 'detail.html?id=18',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(159,129,101)',
+            bg2_color:'rgb(41,62,81)',
+            bg1_height:'50%',//版式3
+            bg1_width:'100%',
+            bg2_height:'50%',
+            bg2_width:'100%',
+            bg2_left:'0%',
+            bg2_top:'50%',
         },
         {
         	genre:'illustion',
             imgUrl: './images/illustion/9_index.png',
             imgTitle: '街角 （下）',
-            linkUrl: 'detail.html?id=26'
+            linkUrl: 'detail.html?id=26',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(243,237,229)',
+            bg2_color:'rgb(237,98,121)',
+            bg1_height:'50%',//版式3
+            bg1_width:'100%',
+            bg2_height:'50%',
+            bg2_width:'100%',
+            bg2_left:'0%',
+            bg2_top:'50%',
         },
         {
         	genre:'illustion',
             imgUrl: './images/illustion/2_index.png',
             imgTitle: '下午茶',
-            linkUrl: 'detail.html?id=19'
+            linkUrl: 'detail.html?id=19',
+            bg3_color:'rgb(239,174,227)',
+            bg1_color:'rgb(254,252,180)',
+            bg2_color:'rgb(254,157,124)',
+            bg1_height:'50%',//版式1
+            bg1_width:'100%',
+            bg2_height:'50%',
+            bg2_top:'50%',
         },
         {
         	genre:'illustion',
             imgUrl: './images/illustion/3_index.png',
             imgTitle: '哦 噗嗤',
-            linkUrl: 'detail.html?id=20'
+            linkUrl: 'detail.html?id=20',
+            bg3_color:'rgb(254,252,202)',
+            bg1_color:'rgb(213,233,217)',
+            bg2_color:'rgb(0,171,71)',
+            bg1_height:'50%',//版式1
+            bg1_width:'100%',
+            bg2_height:'50%',
+            bg2_top:'50%',
         },
         {
         	genre:'illustion',
             imgUrl: './images/illustion/4_index.png',
             imgTitle: '插画',
-            linkUrl: 'detail.html?id=21'
+            linkUrl: 'detail.html?id=21',
+            bg3_color:'rgb(254,252,202)',//版式4
         },
         {
         	genre:'illustion',
             imgUrl: './images/illustion/5_index.png',
             imgTitle: '蘑菇人',
-            linkUrl: 'detail.html?id=22'
+            linkUrl: 'detail.html?id=22',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(119,164,213)',
+            bg2_color:'rgb(49,76,153)',
+            bg1_height:'50%',//版式3
+            bg1_width:'100%',
+            bg2_height:'50%',
+            bg2_width:'100%',
+            bg2_left:'0%',
+            bg2_top:'50%',
         },
         {
         	genre:'illustion',
             imgUrl: './images/illustion/6_index.png',
             imgTitle: '啊',
-            linkUrl: 'detail.html?id=23'
+            linkUrl: 'detail.html?id=23',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(36,39,54)',
+            bg2_color:'rgb(217,218,222)',
+            bg1_height:'100%',//版式2
+            bg2_height:'100%',
+            bg2_top:'0%',
         },
         {
         	genre:'illustion',
             imgUrl: './images/illustion/7_index.png',
             imgTitle: '随笔（上）',
-            linkUrl: 'detail.html?id=24'
+            linkUrl: 'detail.html?id=24',
+            bg3_color:'rgb(255,214,234)',
+            bg1_color:'rgb(119,164,213)',
+            bg1_height:'50%',
+            bg1_width:'100%',
+            bg2_color:'rgb(49,76,153)',//版式1
+            bg2_height:'50%',
+            bg2_top:'50%',
         },
         {
         	genre:'illustion',
             imgUrl: './images/illustion/8_index.png',
             imgTitle: '随笔（下）',
-            linkUrl: 'detail.html?id=25'
+            linkUrl: 'detail.html?id=25',
+            bg3_color:'rgb(237,98,121)',//版式4
         },
         {
         	genre:'illustion',
             imgUrl: './images/illustion/10_index.png',
             imgTitle: '井上雄彦',
-            linkUrl: 'detail.html?id=27'
+            linkUrl: 'detail.html?id=27',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(36,39,54)',
+            bg2_color:'rgb(217,218,222)',
+            bg1_height:'50%',//版式3
+            bg1_width:'100%',
+            bg2_height:'50%',
+            bg2_width:'100%',
+            bg2_left:'0%',
+            bg2_top:'50%',
         },
         {
         	genre:'illustion',
             imgUrl: './images/illustion/11_index.png',
             imgTitle: '自画像',
-            linkUrl: 'detail.html?id=28'
+            linkUrl: 'detail.html?id=28',
+            bg3_color:'rgb(242, 242, 242)',
+            bg1_color:'rgb(0,171,71)',
+            bg2_color:'rgb(213,233,217)',
+            bg1_height:'50%',//版式3
+            bg1_width:'100%',
+            bg2_height:'50%',
+            bg2_width:'100%',
+            bg2_left:'0%',
+            bg2_top:'50%',
         }
 ];
