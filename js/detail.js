5af767fb6a7a26168264efc6e8d257946e4822b1
ltjn <364125168@qq.com>
2018-07-18 18:14:13 +0800

    update description

--- js/detail.js
+++ js/detail.js
@@ -3,7 +3,7 @@ var detailData = [
         index:0,
         title: "博达环境导视系统",
         descriptionChinese: "为了提升公司的品牌形象，对公司的整体内部环境进行了统一的导视设计，我认为导视系统应该更自然的融入到当前的环境和建筑本身，根据公司的标准色，蓝色和橙色、以及公司的行业属性，对其进行延展， 提出了以下两个方案",
-        descriptionEnglish: "In order to enhance the brand image of the company, the company's overall internal environment is the unity of advertising design, I think advertising system should be more naturally into the current environment and the building itself, according to the company standard color, blue and orange, attributes of the industry and the company, to extend, the following two schemes are put forward",
+        descriptionEnglish: "The role of sign is to provide people with signal, instruction and indication, mainly focus on the relationship between people and environment, strengthening the characteristics of brand recognition. I think sign system should be more naturally involved into current environment or architecture. <br>According to professional nature of the company and software industry, sign also has the function of line guide, which decides the basic structure elements and technology wire, having characteristics of controlling walking rote and trajectory.",
         imgs: [
             "./images/communication/one/1.jpg",
             "./images/communication/one/2.jpg",
@@ -70,7 +70,7 @@ var detailData = [
         index:5,
         title: "TEDx XI’AN",
         descriptionChinese: "TEDx Xi’an是一个非营利的公益性组织，自己有幸成为其中的一名志愿者，负责大会的整体的视觉海报及延伸品，视觉组的小伙伴都很有才，各尽其能，在这个大家庭里，没有甲方的干预，可以大胆的解放天性，大开脑洞。",
-        descriptionEnglish: "TEDx Xi'an is a nonprofit public service organization, have the opportunity to become a volunteer yourself which, overall visual poster and extension products, a small group of partners in charge of the General Assembly of the visual is very talented, do their best, in this family, no party intervention can boldly liberate nature, wide open brain hole.",
+        descriptionEnglish: "TED x Xi’an is a non-profit organization. It is so lucky that I could be one of the volunteers and responsible for the visual poster and extension. The partners in visual group are quite gifted and have their own abilities. In this family, there is no intervene, so we can release our imagination.",
         imgs: [
             "./images/communication/six/1.png",
             "./images/communication/six/2.png",
@@ -102,7 +102,7 @@ var detailData = [
         index:7,
         title: "华阳景区",
         descriptionChinese: "华阳景区是秦岭山中一座4A及景区，这个项目主要是推端午节华阳当地举办的活动，做的线上线下的一系列数字传播，以当地一些特色为主要元素，有秦岭四宝，30年从来不打农药的大米等，传播量是他们往期的小高潮。",
-        descriptionEnglish: "Huayang resort is Qinling Mountains and a 4A scenic spots, this project is to promote local events Huayang Dragon Boat Festival, a series of numbers to do online and offline communication, with some local specialties as the main element, Qinling four treasures, 30 never fight in rice pesticide, spread to the amount of their spurt.",
+        descriptionEnglish: "Huayang Scenic Spot is a 4A scenic spot located at Qinling Mountain. This is a project to promote local events during Dragon Boat Festival, holding a series of digital communication online and offline with some local specialties as the main element, including Qinling Four Treasures, rice without pesticide for 30 years. The amount of transmission is their last small climax.",
         imgs: [
             "./images/ui-design/one/1.gif",
             "./images/ui-design/one/2.gif",
@@ -114,7 +114,7 @@ var detailData = [
         index:8,
         title: "西安音乐厅文艺账单h5",
         descriptionChinese: "西安音乐厅文艺账单倒计时h5，与支付宝的年度账单有异曲同工之意，根据音乐厅的品牌属性，选用了世界名画与现代科技和场景进行拼接，形成蒙太奇手法，丰富了表现力，增强观者的感染性。",
-        descriptionEnglish: "Xi 'an concert hall literary bills countdown h5, and alipay annual bills have different meaning, the brand property, according to the concert hall chose world famous paintings with modern science and technology and the scene splicing, form a montage, enriched the expressive force, strengthen the viewer infectious.",
+        descriptionEnglish: "The countdown h5 literary bill of Xi’an Concert Hall, which has a equally satisfactory results with Alipay. According to the brand of concert hall, we choose world famous paintings, combined with modern technology and scene to form a montage expression, enriching the expression and strengthening the infections. ",
         imgs: [
             "./images/ui-design/two/1.png"
         ]
@@ -123,7 +123,7 @@ var detailData = [
         index:9,
         title: "长安嘿帮",
         descriptionChinese: "长安嘿帮是一个媒体公众号，旨在复兴古城新青年文化计划，做了整体的品牌视觉系统，logo即是眼睛的意思，还有领域、地域的意思，传播古城西安不一样的声音。",
-        descriptionEnglish: "Changan hey help is a media to the public, to plan to revive the ancient city of new youth culture, make the overall brand vision system, the logo is the meaning of the eyes, and field, the meaning of geographical, spread the ancient city of xi 'an is not the same.",
+        descriptionEnglish: "Chang’an Hey Help is an Official Account, aiming at reviving new youth culture plan at the old city and making a vision system for the whole brand. The logo indicates both eyes and areas, broadcasting the different voice of Xi’an. ",
         imgs: [
             "./images/ui-design/three/1.png",
             "./images/ui-design/three/2.png"
@@ -134,7 +134,7 @@ var detailData = [
         title: "D.E.A.L",
         descriptionChinese: "凡生素影是一家专门拍肖像的专业摄影机构，这个项目是为摄影机构开发微信小程序的一个线上预约、下单，提供服务与展示作品的一个窗口。<br>" +
         "用户可以通过小程序选择自己需要的套系，查看详情，并能预约下单，进行购买服务。",
-        descriptionEnglish: "D.E.A.L is one of the best portrait professional photography agency, this project is for photography organization development micro letter small program of an online reservation, order, provide service and display a window. The user can select the set system that he needs through the small program, check the details, and can make an appointment to order and purchase the service." +
+        descriptionEnglish: " D.E.A.L is one of the best portrait professional photography. This project is a window for photographers to develop an online reservation and place orders at WeChat Mini Program, providing services and displaying works. Users could select what he need, check details, make an appointment and purchase service through the mini program." +
         "<br>设计思考:此摄影机构的装修风格及调性是以黑白灰的极简北欧风为住，所以设计的方向及颜色的选用也采用简洁、清晰、主次分明的线框与黑白灰的主色调，统一视觉形象，加深对品牌的记忆。图标:轻量化、统一性、易识别。页面减少不必要的元素，保持干净清晰。\n" +
         "同时突出品牌的极简概念，去繁从简，去除引导页，直接展示主要产品。",
         imgs: [
@@ -146,7 +146,7 @@ var detailData = [
         index:11,
         title: "飞跃大西安",
         descriptionChinese: "西安城墙五一吉尼斯，千架无人机营销活动h5，通过无人机飞行碰撞宝藏，赢得活动奖励，并绘制了西安城墙极具代表性的建筑为障碍物，通过游戏互动，让市民参与到活动中。",
-        descriptionEnglish: "Xi 'an city wall May Day guinness, thousand drone h5 marketing activities, and through the unmanned aerial vehicle (uav) flight collision treasure, award winning activity, and draw the obstacles for xi 'an city wall is extremely representative buildings, through the interactive game, let the citizens to participate in the activities.",
+        descriptionEnglish: " Xi’an City Wall May Day Guinness is a marketing activity h5 of thousands of unmanned aerial vehicle. Through the UAV flight collision treasure, people win the activity reward, and draw the Xi'an city wall extremely representative building as the barrier, let the citizen participate in the activity by game interaction.",
         imgs: [
             "./images/ui-design/five/1.png",
             "./images/ui-design/five/2.png",
@@ -194,7 +194,7 @@ var detailData = [
         index:15,
         title: "西安城墙海报",
         descriptionChinese: "西安城墙的环境改造以大胆跳跃的蓝黄色调为主旋律，提升因采光不足带来的心情烦躁等问题，依照城墙的口号，尊重、复兴、热爱，表达西安城墙景区即尊重历史，发扬传统文化，同时又与国际的现代化相结合，体现城墙景区肩负责任，传承创新，追求卓越，共同发展的理念特征。",
-        descriptionEnglish: "Environmental transformation Xi'an city wall with a bold leap of blue and yellow as the theme tune to improve due to insufficient lighting brings feelings of irritability and other issues, according to the slogan of walls, respect, revival, love, expressing respect for the history of Xi'an City resorts, carry forward the traditional culture, while combining modern and international, reflecting the city wall scenic shoulder responsibility, inheritance and innovation, the pursuit of excellence and common development of the concept features",
+        descriptionEnglish: "The environmental Reform of Xi’an City Wall takes blue and yellow as theme melody, solving the problems like being in fidgety mood due to insufficient lighting. According to the slogan of City Wall, Respect, Revive and Love, indicating Xi’an City Wall not only respects to history and carries forward traditional culture, but also combined with international modernization, which reflects the concept of City Wall Scenic area shouldering responsibility, inheriting and innovating, pursuing Excellence and Common Development.",
         imgs: [
             "./images/poster-design/one/1.jpg",
             "./images/poster-design/one/2.jpg",
@@ -221,7 +221,7 @@ var detailData = [
         index:17,
         title: "我是赫拉话剧海报",
         descriptionChinese: "课题是一部由西安戏剧中心推出的首部启蒙话剧，我是赫拉，为了让孕期的产妇以轻松快乐的心情迎接新生儿的到来，认为怀孕是快乐的事儿，不是什么病，所以形象以挺着大肚子的孕妇欢快的跳着小苹果，来体现即将将临的准妈妈的喜悦和无忧的保障。",
-        descriptionEnglish: "Issue is a Xi'an Drama Center launched the first section of enlightenment drama, I was Hera, in order to allow pregnant mothers to relaxed and happy mood to greet the arrival of the newborn that pregnancy is a happy thing, not a disease, so the image to pregnant women belly dancing cheerful small apples, to reflect the joy and worry-free security upcoming Advent of mothers.",
+        descriptionEnglish: "The topic is the first enlightening drama produced by Xi’an Drama Center. The aim of this drama is to make pregnant women welcome their children with relax, thinking that pregnant is a pleasure thing rather than sick. Therefore, the image of ‘I am Hera’ is pregnant women who are dancing small apples, indicating joy and worry-free security for the woman who will be a mother.",
         imgs: [
             "./images/poster-design/three/1.jpg",
             "./images/poster-design/three/2.jpg",
@@ -331,7 +331,7 @@ var detailData = [
         index:29,
         title: "send a letter",
         descriptionChinese: "",
-        descriptionEnglish: "",
+        descriptionEnglish: "‘Send A Letter’ concept books is notes about adolescence, recording my growth from high school to university, which is a memory notebook to my love people and people who love me. This book collects my illustration and daily record, which is also a memory of my adolescent hormone. It is suitable for people who still nostalgia for the lost youth.",
         imgs: [
             "./images/communication/eight/1.JPG",
             "./images/communication/eight/2.JPG",
@@ -359,7 +359,7 @@ var detailData = [
         index:31,
         title: "字体实验",
         descriptionChinese: "",
-        descriptionEnglish: "",
+        descriptionEnglish: "An unexpected exhibition experience triggered my interest towards a poster of the Anti-Japan War and made me to do this font experiment. This set of font has a style in the period of Republic of China, using the geometric exact form. There is a form of advertising font, namely pop font, retaining the unique charm of Chinese characters. The glyph design is concise and succinct, conforming to the characteristics of modernism. Based on this, every painting has a different way of expressions, and this variable form is more expressive; every word has many forms and rules.",
         imgs: [
             "./images/communication/ten/1.jpg",
         ]
